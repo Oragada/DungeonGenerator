3479305d4a80dd0f5b3422b97ea0676a9965762a
Oragada <Oragada@gmail.com>
2014-09-04 16:41:39 +0200

    Initial commit
    initial genotype and dijkstra implementation

---
+++ DungeonGenerator/Dijkstra.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DungeonGenerator
+{
+    public class Dijkstra
+    {
+        public static bool Run(Dungeon dungeon)
+        {
+            Dictionary<Room, int> distanceMap = new Dictionary<Room, int>();
+            Dictionary<Room, bool> visited = new Dictionary<Room, bool>();
+            foreach (Room room in dungeon.rooms)
+            {
+                distanceMap.Add(room,int.MaxValue);
+                visited.Add(room,false);
+            }
+            //Assume all rooms are unique
+            //Assume all rooms cannot link to themselves
+            //Assume all connections are unique
+            distanceMap[dungeon.Entrance] = 0;
+            Room current = dungeon.Entrance;
+            while (true)
+            {
+                //get all neighbor rooms:
+                //get connections from the current room,
+                //then get the room at the other end
+                //that is not on the visited list
+                IEnumerable<Room> neighborRooms =
+                    dungeon.Connections.Where(c => c.containsRoom(current))
+                        .Select(e => e.getOtherRoom(current))
+                        .Where(e => !visited[e]);
+                foreach (Room neighborRoom in neighborRooms.Where(neighborRoom => distanceMap[current] + 1 < distanceMap[neighborRoom]))
+                {
+                    distanceMap[neighborRoom] = distanceMap[current] + 1;
+                }
+
+                visited[current] = true;
+                if (visited[dungeon.Exit])
+                {
+                    return true;
+                }
+                //Does any room exists that is not visited and is neighbouring a visited room (has a tentative distance) 
+                if (distanceMap.Any(e => distanceMap[e.Key] != int.MaxValue & !visited[e.Key]))
+                {
+                    return false;
+                }
+                current = distanceMap.First(e => e.Value == distanceMap.Min(f => f.Value) && !visited[e.Key]).Key;
+            }
+        }
+    }
+}
---
+++ DungeonGenerator/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DungeonGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    //Path
+
+    //Eval strategy
+    /*
+     * Can we get from start to finish? (Path)
+     * How many rooms can the player beat? (Challenge level)
+     * How much of the run is unchallenging? (Rooms where the player is not challenged)
+     * (Treasure)
+     */
+
+    //Genotype
+    public class Dungeon
+    {
+        public readonly List<Room> rooms;
+        
+        public readonly Room Entrance;
+
+        public readonly Room Exit;
+
+        public readonly List<Connection> Connections;
+
+        public Dungeon(List<Room> rooms, List<Connection> roomConnections, Room entrance, Room exit)
+        {
+            this.rooms = rooms;
+            this.Entrance = entrance;
+            this.Exit = exit;
+            this.Connections = roomConnections;
+        }
+
+
+
+
+    }
+
+    public class Connection
+    {
+        public Room oneRoom;
+        public Room otherRoom;
+
+        public Room getOtherRoom(Room r)
+        {
+            if (r == oneRoom) return otherRoom;
+            if (r == otherRoom) return oneRoom;
+            return null;
+        }
+
+        public bool containsRoom(Room r)
+        {
+            if (r == oneRoom || r == otherRoom) return true;
+            return false;
+        }
+    }
+
+    public class Room
+    {
+        List<Enemy> enemies = new List<Enemy>(); 
+        //List<Treasure> loot = new List<Treasure>();
+        private int sizeX, sizeY;
+    }
+
+    internal enum Enemy
+    {
+        Rat = 1, Goblin = 3, Wolf = 4, Orc = 6, Bear = 8, Ogre = 18, Dragon = 30  
+    }
+}
